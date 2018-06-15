2a36319f1dc3a40bbd10f4a7888846b9fc05c887
prof79 <markus@markusegger.at>
2018-06-15 22:27:42 +0200

    Cosmetic changes.

--- TheC64Disker/Views/DiskSelectionView.xaml.cs
+++ TheC64Disker/Views/DiskSelectionView.xaml.cs
@@ -7,17 +7,12 @@
 // <description>
 //      Interaction logic for DiskSelectionView.xaml.
 // </description>
-// <version>v0.9.0 2018-06-13T00:11:00+02</version>
+// <version>v0.9.0 2018-06-15T22:27:00+02</version>
 //----------------------------------------------------------------------------
 
 namespace at.markusegger.Application.TheC64Disker.Views
 {
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel;
-    using System.Windows;
     using System.Windows.Controls;
-    using System.Windows.Media;
 
     /// <summary>
     /// Interaction logic for DiskSelectionView.xaml
@@ -25,8 +20,6 @@ namespace at.markusegger.Application.TheC64Disker.Views
     public partial class DiskSelectionView : UserControl
     {
         public DiskSelectionView()
-        {
-            InitializeComponent();
-        }
+            => InitializeComponent();
     }
 }
